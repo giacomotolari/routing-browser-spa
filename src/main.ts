d850723ae120ca2673b7b727a412313745fc0ff7
giacomotolari <tolarigiacomo@gmail.com>
2025-01-15 10:56:39 +0100

    refactor: simplify navigation links and implement client-side routing

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,28 @@ const currentPath = window.location.pathname;
 console.log("🚀 ~ currentPath:", currentPath);
 
 const appEl = document.querySelector<HTMLDivElement>("#app");
-const h1El = document.createElement("h1");
 
-if (currentPath === "/") {
-  h1El.textContent = "Home";
-} else if (currentPath === "/contact") {
-  h1El.textContent = "Contact";
-} else {
-  h1El.textContent = "Not found!";
-}
+const navLinksElements = document.querySelectorAll("a");
 
-appEl?.appendChild(h1El);
+navLinksElements.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const targetPath = (event.target as HTMLAnchorElement).getAttribute("href");
+    console.log("🚀 ~ link.addEventListener ~ targetPath:", targetPath);
+    if (targetPath) {
+      history.pushState({}, "", targetPath);
+    }
+  });
+});
+
+// const h1El = document.createElement("h1");
+
+// if (currentPath === "/") {
+//   h1El.textContent = "Home";
+// } else if (currentPath === "/contact") {
+//   h1El.textContent = "Contact";
+// } else {
+//   h1El.textContent = "Not found!";
+// }
+
+// appEl?.appendChild(h1El);
