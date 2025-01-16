dca69b3ddad0f6f05d3447fd2b7131aea2482157
giacomotolari <tolarigiacomo@gmail.com>
2025-01-16 10:59:59 +0100

    refactor: enhance router functionality and clean up about page code

--- src/features/navigation/router.ts
+++ src/features/navigation/router.ts
@@ -1,21 +1,36 @@
 import { routes } from "./routes";
 
+/**
+ * Updates the content of the application based on the current path.
+ *
+ * This function retrieves the current path from the window location,
+ * finds the corresponding route from the `routes` array, and updates
+ * the inner HTML of the provided `appEl` element with the content
+ * fetched from the route's page function.
+ *
+ * @param {HTMLDivElement} appEl - The HTML element where the content will be updated.
+ */
 async function updateAppContent(appEl: HTMLDivElement) {
   const currentPath = window.location.pathname;
+
   let content = "";
+
+  /**
+   * Finds the route that matches the current path or a wildcard route.
+   *
+   * This function searches through the `routes` array to find a route object
+   * whose `path` property matches the `currentPath`. If no such route is found,
+   * it falls back to finding a route with a wildcard path ("*").
+   *
+   * @param routes - An array of route objects, each containing a `path` property.
+   * @param currentPath - The current path to match against the routes.
+   * @returns The matched route object or the wildcard route object if no match is found.
+   */
   const route =
     routes.find((route) => route.path === currentPath) ||
     routes.find((route) => route.path === "*");
+
   content = await route!.page();
-  //   if (currentPath === "/") {
-  //     content = await homePage();
-  //   } else if (currentPath === "/contact") {
-  //     content = await contactPage();
-  //   } else if (currentPath === "/about") {
-  //     content = await aboutPage();
-  //   } else {
-  //     content = await notFoundPage();
-  //   }
 
   appEl!.innerHTML = content;
 }
--- src/pages/about.ts
+++ src/pages/about.ts
@@ -1,8 +1,7 @@
 async function aboutPage() {
-    const response = await fetch("/src/pages/html/about.html");
-    const html = await response.text();
-    return html;
-  }
-  
-  export default aboutPage;
-  
+  const response = await fetch("/src/pages/html/about.html");
+  const html = await response.text();
+  return html;
+}
+
+export default aboutPage;
